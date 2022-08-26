3fb262c8de8ddca1a40b92a405be283bddc019b5
davizinho98 <daviseles99@gmail.com>
2022-08-26 16:00:54 -0500

    feat: implementa middleware de validação de login

---
+++ src/middlewares/validateDataLogin.js
@@ -0,0 +1,11 @@
+const validateDataLogin = (request, response, next) => {
+  const { email, password } = request.body;
+
+  if (!email || !password) {
+    return response.status(400).json({ message: 'Some required fields are missing' });
+  }
+
+  next();
+};
+
+module.exports = validateDataLogin;
