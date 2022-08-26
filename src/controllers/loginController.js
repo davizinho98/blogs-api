8fcd7ff0dc0244f396b5ccbe2027aef3a899a253
davizinho98 <daviseles99@gmail.com>
2022-08-26 16:01:46 -0500

    feat: implementa controller de login retornando token ou erro caso não houver no banco

---
+++ src/controllers/loginController.js
@@ -0,0 +1,14 @@
+const loginService = require('../services/loginService');
+
+const getTokenLogin = async (request, response) => {
+  try {
+    const token = await loginService.getTokenLogin(request.body);
+
+    response.status(200).json({ token });
+  } catch (error) {
+    response.status(400).json({ message: error.message });
+  };
+
+};
+
+module.exports = { getTokenLogin };
