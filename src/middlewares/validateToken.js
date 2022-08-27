af818d44862590161793b7fa51f8a1384498f0d9
davizinho98 <daviseles99@gmail.com>
2022-08-27 07:40:49 -0500

    feat: implementa middleware de validação de token

---
+++ src/middlewares/validateToken.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../database/models');
+
+const { JWT_SECRET } = process.env;
+
+const validateToken = async (request, response, next) => {
+  const token = request.headers.authorization;
+
+  if (!token) {
+    return response.status(401).json({ message: 'Token not found' });
+  }
+
+  try {
+    const { displayName } = jwt.verify(token, JWT_SECRET);
+
+    const user = await User
+      .findOne({ where: displayName });
+
+    if (!user) {
+      return response.status(401).json({ message: 'Expired or invalid token' });
+    }
+
+    request.user = user;
+
+    next();
+  } catch (error) {
+    return response.status(401).json({ message: error.message });
+  }
+};
+
+module.exports = validateToken;
