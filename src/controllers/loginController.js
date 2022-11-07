61daefc81b94f70487058d710e8905f3221a8eb5
davizinho98 <daviseles99@gmail.com>
2022-11-07 15:01:42 -0500

    feat: add return before response

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,9 +4,9 @@ const getTokenLogin = async (request, response) => {
   try {
     const token = await loginService.getTokenLogin(request.body);
 
-    response.status(200).json({ token });
+    return response.status(200).json({ token });
   } catch (error) {
-    response.status(400).json({ message: error.message });
+    return response.status(400).json({ message: error.message });
   }
 };
 
